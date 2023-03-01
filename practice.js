d672ee769c6a46d896a76f37ff50510dbafd38d6
Anita-Sheptytska <anita.sheptytska@gmail.com>
2023-03-01 18:56:44 +0200

    1.03

---
+++ practice.js
@@ -0,0 +1,17 @@
+// const officialName = prompt("What the official name JS?")
+
+// if (officialName === ECMAScript) {
+//     alert("Good job")
+// } else {
+//     alert("Oh, no..It's ECMAScript")
+// }
+
+
+// function takeTime (number) {
+//     const hour = Math.floor(number / 60);
+//     const min = number % 60;
+
+//     console.log(`${hour}:${min}`);
+// }
+//  takeTime(1678)
+
