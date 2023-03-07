4c9fbcc8be5b2d49ba2974081bb1f36a5556e0e3
Anita-Sheptytska <anita.sheptytska@gmail.com>
2023-03-07 23:11:04 +0200

    7.03

--- practice.js
+++ practice.js
@@ -7,6 +7,7 @@
 // }
 
 
+
 // function takeTime (number) {
 //     const hour = Math.floor(number / 60);
 //     const min = number % 60;
@@ -14,4 +15,21 @@
 //     console.log(`${hour}:${min}`);
 // }
 //  takeTime(1678)
+// const askMinutes = prompt("pls enter minutes")
+// takeTime(askMinutes)
+
+
+
+// function numberDegrees(min, max) {
+//         for (let i = max; i >= min; i -= 1)
+//             console.log(i);
+//     }
+//     for (let i = min; i <= max; i += 1) {
+//         if (i % 2 === 0) {
+//             console.log(i)
+//         }
+//     }
+
+// numberDegrees(1, 20);
+
 
